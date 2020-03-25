dadcc466e3f38de9f89b126fd6a4154838994bad
Brett Bender <brett@brettb.xyz>
2020-03-25 18:09:55 -0500

    Shimmy a little bit around, add a readme.

---
+++ README.md
@@ -0,0 +1,45 @@
+# ApolloCore
+
+A Bukkit plugin framework by Brett Bender
+
+This library aims to make writing a plugin quicker and easier.
+
+## Installation
+
+
+### Maven
+```xml
+<repository>
+    <id>brettb-repo</id>
+    <url>https://repo.brettb.xyz</url>
+</repository>
+```
+
+```xml
+<dependency>
+    <groupId>xyz.brettb</groupId>
+    <artifactId>ac</artifactId>
+    <version>1.0.0</version>
+    <scope>provided</scope>
+</dependency>
+```
+
+### Gradle
+```gradle
+repositories {
+    maven {
+        name = 'brettb-repo'
+        url = 'https://repo.brettb.xyz'
+    }
+}
+
+dependencies {
+    compileOnly group: 'xyz.brettb', name: 'ac', version: '1.0.0'
+}
+```
+
+After that, just make sure to include a built ApolloCore in your `plugins` folder.
+
+## Usage
+
+Documentation is W.I.P.
--- src/main/java/xyz/brettb/ac/ac.java
+++ src/main/java/xyz/brettb/ac/ac.java
@@ -1,7 +1,31 @@
 package xyz.brettb.ac;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import xyz.brettb.ac.commands.CorePluginCommand;
+import xyz.brettb.ac.commands.CorePluginCommandMeta;
+import xyz.brettb.ac.plugin.CorePlugin;
 
-public class ac extends JavaPlugin {
+public class ac extends CorePlugin {
+
+    @Override
+    public void onModuleEnable() {
+        registerCommand(new ApolloCoreCommand());
+    }
+
+    @CorePluginCommandMeta(description = "ApolloCore", aliases = {"ac"})
+    private static class ApolloCoreCommand extends CorePluginCommand {
+
+        public ApolloCoreCommand() {
+            super("apollocore");
+        }
+
+        @Override
+        public void handleCommandUnspecific(CommandSender sender, String[] args) {
+            sender.sendMessage(ChatColor.AQUA + "[AC] " + ChatColor.DARK_AQUA + "This server is using ApolloCore" +
+                    ChatColor.GREEN + " v" + getPlugin().getDescription().getVersion());
+        }
+
+    }
 
 }
